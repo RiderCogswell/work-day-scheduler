47c65e0f730c9a1b2d6fd8ea9a96a038ee5a5a4e
Ridercogswell <ridercogswell@gmail.com>
2022-01-30 16:30:32 -0800

    add text area to hours that are available

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 
 // current date
 var today = moment()
-$("#currentDay").text(today.format("dddd, MMMM, YYYY, h:mm:ss a"));
+$("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 // tasks arrays for local storage
 
 var tasks = {
@@ -15,12 +15,12 @@ var tasks = {
     "15": [],
     "16": [],
     "17": []
-}
+};
 
 // add task to local storage
 var setTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks))
-}
+};
 
 var getTasks = function() {
     // load task from local storage 
@@ -36,7 +36,7 @@ var getTasks = function() {
         })
     }
 
-}
+};
 
 // create task function
 
@@ -46,4 +46,43 @@ var createTask = function(taskText, hourDiv) {
     .addClass("description")
     .text(taskText)
     taskDiv.html(taskP);
-}
+};
+
+// replace text area
+var replaceText = function(textAreaEl) {
+    var taskInfo = textAreaEl.closest(".task-info");
+    var textArea = taskInfo.find("textarea");
+
+    // set variables 
+    var time = taskInfo.attr("id");
+    var text = textArea.val();
+
+    // persist data through local storage
+    tasks[time] = [text];
+
+    // call createTask to replace textarea 
+    createTask(text, taskInfo);
+};
+
+// add event listeners
+
+$(".task").click(function() {
+    $("textarea").each(function() {
+        replaceText($(this));
+    })
+
+    // set time variable
+    var time = $(this).closest(".task-info").attr("id");
+    // if parse.int(time) greater than current moment
+    if (parseInt(time) >= moment().hour()) {
+        var text = $(this).text();
+        // create text area with needed text
+        var textInput = $("<textarea>")
+        .addClass("form-control")
+        .val(text);
+        // add text input el to parent div
+        $(this).html(textInput);
+        textInput.trigger("focus");
+    }
+})
+
